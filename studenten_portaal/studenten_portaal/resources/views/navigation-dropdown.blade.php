aa78821cf79fd5c03c249e889898386d4bfaeab2
Rutger Vos <99033234@mydavinci.nl>
2020-11-18 11:56:51 +0100

    added base building blocks for Q&A

--- studenten_portaal/studenten_portaal/resources/views/navigation-dropdown.blade.php
+++ studenten_portaal/studenten_portaal/resources/views/navigation-dropdown.blade.php
@@ -11,9 +11,15 @@
                 </div>
 
                 <!-- Navigation Links -->
+                <x-jet-nav-link href="{{ route('dashboard') }}">
+                        {{ __('Homepage') }}
+                    </x-jet-nav-link>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('dashboard') }}">
+                        {{ __('Q&A') }}
                     </x-jet-nav-link>
                 </div>
             </div>
